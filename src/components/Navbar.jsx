b2f737dae1d59652e1ad317a9514406c5ca22e0f
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-28 22:53:58 +0600

    Almost done with navbar, Trying Material-icons for the first time, it's good

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
+import { Badge } from '@material-ui/core';
+import { LocalMallRounded, Search } from '@material-ui/icons';
+import React from 'react';
+import styled from 'styled-components';
 
 const Container = styled.div`
     height: 60px;
@@ -10,14 +12,77 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
     padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+
+const Language = styled.div`
+    font-size: 40px;
+`;
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
+
+const Center = styled.div` 
+    flex: 1;
+    text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
+
+const MenuItem = styled.div`
+    font-size : 24px;
+    cursor: pointer;
 `;
 
 const Navbar = () => {
-  return (
-    <Container>
-        <Wrapper>Navbar</Wrapper>
-    </Container>
-  )
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>English</Language>
+                    <SearchContainer>
+                        <Input/>
+                        <Search/>
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>Bettermart</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>SignIn</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color='primary'>
+                            <LocalMallRounded/>
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
+    )
 }
 
 export default Navbar
