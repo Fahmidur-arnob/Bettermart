e7ef4f130dc4ad5421e33915bf911b90251006ec
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-29 00:56:30 +0600

    Added a transition animation in slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -33,6 +33,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
     height: 100%;
     display: flex;
+    transition: all 1.5s ease;
     transform: translateX(${props=>props.slideIndex * -100}vw);//because we're going to move the slider on the x-axis;
 `;
 
